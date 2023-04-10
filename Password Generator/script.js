0e8087d17993411b27348e351faa4882aca81a5a
MovieDeko <dosomethinginurlife@gmail.com>
2023-04-10 21:10:54 +0530

    chaged password generater

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -3,7 +3,7 @@ const lowerSet = "abcdefghijklmnopqrst";
 const  symble = "!@#$%^&*()-=_+[]{};':,./<>?";
 const num = "123456789";
 
-const length = document.getElementById("length");
+const len = document.getElementById("length");
 const upper = document.getElementById("upper");
 const lower = document.getElementById("lower");
 const sym = document.getElementById("symbole");
@@ -12,29 +12,39 @@ const outPut = document.getElementById("display");
 
 const randPass1 = (dataSet) =>{return dataSet[Math.floor(Math.random() * dataSet.length)]; } 
 
-const generatedPass = (pass = "") => {
+function generatedPass(pass = "") {
 
-if(pass.length > length.value)
-    console.log("more");
+    // if the password length greater then entered number the trim 
+    if(pass.length > len.value)
+        return pass.slice(0,len.value);
 
-if(upper.checked){
-    pass += randPass1(upperSet);
+    if (upper.checked) {
+        pass += randPass1(upperSet);
+    }
+    if (lower.checked)
+        pass += randPass1(lowerSet);
+
+    if (sym.checked)
+        pass += randPass1(symble);
+
+    if (number.checked)
+        pass += randPass1(num);
+
+    if (pass.length < len.value)
+        return generatedPass(pass);
+
+    console.log(pass);
+    outPut.value = pass;
 }
-if(lower.checked)
-    pass += randPass1(lowerSet);
 
-if(sym.checked)
-    pass += randPass1(symble);
-
-if(number.checked)
-    pass += randPass1(num);
-
-if(pass.length < length.length)
-    return generatedPass(pass);
-
-console.log(pass);
+function copyToClipboard(){
+    const copy = outPut.value;
+    navigator.clipboard.writeText(copy);
+    alert("copied the value :"+copy);
 }
 
 document.getElementById("generate").addEventListener("click",function() {generatedPass();});
+
+document.getElementById("copy").addEventListener("click",function(){copyToClipboard();});
 
 
